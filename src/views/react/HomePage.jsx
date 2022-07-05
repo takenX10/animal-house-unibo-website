e9a10f5e94b90d8989be6c759a86236d233f91e8
takenX10 <frau.alessandro01@gmail.com>
2022-07-05 11:51:34 +0200

    added draft homepage

---
+++ src/views/react/HomePage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "@/components/Navbar/Navbar";
+
+export default function HomePage(){
+    return(
+        <>
+            <Navbar />
+            <main>
+                <div>
+                    <h2 className="fs-1 text-center p-4 m-3">Dove desideri andare?</h2>
+                </div>
+                <div className="container-fluid">
+                    <div className="row fw-bold fs-2">
+                        <div className="col d-flex my-2 justify-content-center align-items-center">
+                            <button type="button" class="button button-danger text-light w-75 bg-danger p-5 rounded-pill" onClick={()=>{window.location="/games/home"}}>Giochi</button>
+                        </div>
+                        <div className="col d-flex my-2 justify-content-center align-items-center">
+                            <button type="button" class="button button-danger text-light w-75 bg-danger p-5 rounded-pill" onClick={()=>{window.location="/office/home"}}>Front office</button>
+                        </div>
+                        <div className="col d-flex my-2 justify-content-center align-items-center">
+                            <button type="button" class="button button-danger text-light w-75 bg-danger p-5 rounded-pill" onClick={()=>{window.location="/back/home"}}>Back office</button>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
