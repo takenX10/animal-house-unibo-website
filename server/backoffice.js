d3b8ebd2b3625489cf7dec978ea0e8cdace36d56
takenX10 <frau.alessandro01@gmail.com>
2022-08-27 12:03:50 +0200

    added register platform

--- server/backoffice.js
+++ server/backoffice.js
@@ -7,8 +7,6 @@ import bodyParser from 'body-parser';
 var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -24,6 +22,17 @@ let ENDPOINTS = [
         opts:  jsonParser,
         function: officePostLogin
     },
+    {
+        endpoint: "/backoffice/register",
+        method: METHODS.GET,
+        function: officeRegister
+    },
+    {
+        endpoint: "/backoffice/register",
+        method: METHODS.POST,
+        opts:  jsonParser,
+        function: officePostRegister
+    },
 ]
 
 function officeLogin(req, res){
@@ -31,9 +40,28 @@ function officeLogin(req, res){
 }
 
 async function officePostLogin(req, res){
-    let db = DATABASE.connect();
+    const db = DATABASE.connect();
     // Really really really vulnerable way of doing this, TODO: fix
     res.json({"response": (await DATABASE.User.exists(req.body) != null ? true : false)});
+}
+
+function officeRegister(req, res){
+    res.sendFile("./backoffice/register.htm", {root: __dirname});
+}
+
+async function officePostRegister(req, res){
+    const db = DATABASE.connect();
+    if(await DATABASE.User.exists({username:req.body.username}) || req.body.username == ""){
+        res.json({"response":false});
+    }else{
+        // Really really really vulnerable way of doing this, TODO: fix
+        await DATABASE.User.create({
+            username: req.body.username,
+            password: req.body.password,
+            isPoster: false
+        });
+        res.json({"response":true});
+    }
 }
 
 export default { ENDPOINTS };
