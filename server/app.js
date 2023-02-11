093c09216bd6754c0a26b6bca2fe4a4a1808d161
Yonas <yonas12@outlook.it>
2023-02-11 16:05:41 +0100

    x gian

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@
 
 import fs from "fs";
 import os from "os";
-import DATABASE from './database.js';
+import DATABASE from "./database.js";
 import initDB from "./init_db.js";
 import multer from "multer";
 import express from "express";
@@ -13,46 +13,49 @@ import cookieParser from "cookie-parser";
 import { __dirname, CLIENT_URL } from "./utils.js";
 import METHODS from "./methods.js";
 
-const upload = multer({ dest: os.tmpdir() });
+const upload = multer({ dest: "./uploads" });
 const app = express();
 const port = 8000;
 
+app.use(express.static(__dirname + "/uploads"));
 app.use(express.static(__dirname + "/static"));
 app.use(express.static(__dirname + "/static/dist"));
 app.use(express.static(__dirname + "/static/dist/assets"));
 app.use(cookieParser());
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/template'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/template"));
 
-var whitelist = ['http://localhost:3000', 'http://localhost:8000']
-var corsOptionsDelegate = function(req, callback) {
+var whitelist = ["http://localhost:3000", "http://localhost:8000"];
+var corsOptionsDelegate = function (req, callback) {
   var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: req.header('Origin'), optionsSuccessStatus: 200, credentials: true } // reflect (enable) the requested origin in the CORS response
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = {
+      origin: req.header("Origin"),
+      optionsSuccessStatus: 200,
+      credentials: true,
+    }; // reflect (enable) the requested origin in the CORS response
   } else {
-    corsOptions = { origin: false } // disable CORS for this request
+    corsOptions = { origin: false }; // disable CORS for this request
   }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 app.use(cors(corsOptionsDelegate));
 
 app.use((err, req, res, next) => {
-  console.log('mistakes were made');
+  console.log("mistakes were made");
   res.status(500).send({ message: err.message });
 });
 
-
-const API_ENDPOINT_PATH = path.join(__dirname, "api")
+const API_ENDPOINT_PATH = path.join(__dirname, "api");
 let backendRouter = [];
 
-
 async function importAPI(api_dir) {
-  let dir = api_dir || API_ENDPOINT_PATH
+  let dir = api_dir || API_ENDPOINT_PATH;
   let files = fs.readdirSync(dir);
   for (let i = 0; i < files.length; i++) {
     try {
-      let path = `${dir}/${files[i]}`
-      let isDir = fs.lstatSync(path).isDirectory()
+      let path = `${dir}/${files[i]}`;
+      let isDir = fs.lstatSync(path).isDirectory();
       if (isDir) {
         await importAPI(path);
         continue;
@@ -62,7 +65,7 @@ async function importAPI(api_dir) {
         backendRouter.push(c.default.ENDPOINTS);
       }
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   }
 }
@@ -70,12 +73,12 @@ async function importAPI(api_dir) {
 function wrapper(func) {
   return async (req, res, next) => {
     try {
-      await func(req, res, next)
+      await func(req, res, next);
     } catch (e) {
       console.log("wajo no eh", e);
       res.status(500).send();
     }
-  }
+  };
 }
 function initAPI() {
   for (let ENDPOINTS of backendRouter) {
@@ -89,8 +92,7 @@ function initAPI() {
         }
       }
       let func = ENDPOINTS[i].function;
-      if (func.constructor.name == 'AsyncFunction')
-        func = wrapper(func)
+      if (func.constructor.name == "AsyncFunction") func = wrapper(func);
       let params = [ENDPOINTS[i].endpoint, opts, func];
       if (ENDPOINTS[i].method == METHODS.GET) app.get(...params);
       else if (ENDPOINTS[i].method == METHODS.POST) app.post(...params);
@@ -110,13 +112,15 @@ function isInRouter(path) {
   return false;
 }
 
-app.post("/upload", upload.single("file"), function(req, res) {
+app.post("/upload", upload.single("file"), function (req, res) {
   console.log("post");
   const file = req.file;
+  console.log(file);
+  console.log(req.body);
   res.json({});
 });
 
-app.get("*", function(req, res, next) {
+app.get("*", function (req, res, next) {
   if (isInRouter(req.url)) {
     return next();
   }
@@ -127,6 +131,6 @@ app.listen(port, async () => {
   console.log(`Animalhouse Backend listening on port ${port}`);
   await DATABASE.connect();
   // await initDB();
-  await importAPI()
+  await importAPI();
   initAPI();
 });
